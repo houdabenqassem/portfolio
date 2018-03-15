e519213aac47e4cf8591e77bf0c6f766e5b817a9
NewUser <houdette.ben@gmail.com>
2018-03-15 14:14:09 -0400

    Add style reset [injectGlobal]

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { injectGlobal } from 'styled-components';
 
-
+injectGlobal`
+body{
+  margin: 0;
+  padding:0;
+  font-family: 'Roboto', sans-serif;
+}
+`;
 ReactDOM.render(<App />, document.getElementById('root'));

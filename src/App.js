103f1c6aa3fffb28f593c272a8e5017c3b320fd7
NewUser <houdette.ben@gmail.com>
2018-03-15 13:26:21 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Random text
+      </div>
+    );
+  }
+}
+
+export default App;

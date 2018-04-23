86cf13adbf07baf748c736f3e3c6812883cdc0dc
NewUser <houdette.ben@gmail.com>
2018-04-23 06:34:10 -0400

    Add new profile picture.

--- src/components/ProfilePicture/index.js
+++ src/components/ProfilePicture/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import cutepic from './houda.jpg';
 import styled from 'styled-components';
 
 const Image = styled.img`
-  height: 200px
-  margin: 20px;
+  height: 330px;
+  width: 200px;
+  margin: 80px 80px 60px;
+  transform: translate(-50%, -50%);
+
 
 `;
 const ProfilePicture = ({className}) => (
 
-  <Image className={className} src={cutepic} />
+  <Image className={className} src='https://s3.amazonaws.com/devhouda/images/houdarev2.gif' />
 );
 
 
